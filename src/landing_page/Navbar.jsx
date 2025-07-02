4fed3542be38e5df4726b387f0640a7259fc98c7
pulkitgaba009 <pulkitgaba009@gmail.com>
2025-07-02 23:14:14 +0530

    made all home components

--- src/landing_page/Navbar.jsx
+++ src/landing_page/Navbar.jsx
@@ -1,7 +1,64 @@
 function Navbar() {
-    return ( 
-        <h1>Navbar</h1>
-     );
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary border-bottom">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src="./logo.svg" alt="" style={{width:"25%",marginLeft:"20%"}}/>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            
+            <form className="d-flex" role="search">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href="#">
+                  SignUp
+                </a>
+              </li>
+              
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Product
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link " href="#">
+                  Pricing
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Support
+                </a>
+              </li>
+
+            </ul>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/landing_page/OpenAccount.jsx
+++ src/landing_page/OpenAccount.jsx
@@ -1,6 +1,12 @@
 function OpenAccount() {
     return ( 
-        <h1>Open Account</h1>
+        <div className="container p-5">
+            <div className="row text-center">
+               <h1 >Open a Zerodha account</h1>
+               <p>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+               <button className="p-2 btn btn-primary fs-5 mb-5" style={{width:"20%" ,margin:"0 auto"}}>Sign up for free</button>
+            </div>
+        </div>
      );
 }
 
