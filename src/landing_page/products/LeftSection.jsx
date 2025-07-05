30892d1feb26a776c587a4c595f7398561da510e
pulkitgaba009 <pulkitgaba009@gmail.com>
2025-07-05 15:32:38 +0530

    Worked on Product Page left section

--- src/landing_page/products/LeftSection.jsx
+++ src/landing_page/products/LeftSection.jsx
@@ -1,7 +1,42 @@
-function LeftSection() {
-    return ( 
-        <h1>Left Section</h1>
-     );
+function LeftSection({
+  imageURL,
+  productName,
+  productDescription,
+  tryDemo,
+  learnMore,
+  googlePlay,
+  appStore,
+}) {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6 p-5">
+          <img src={imageURL} alt="" />
+        </div>
+
+        <div className="col-1"></div>
+
+        <div className="col-5 p-5  mt-5">
+          <h1 className="mb-4">{productName}</h1>
+          <p>{productDescription}</p>
+
+          <div className="mb-4">
+            <a href={tryDemo} style={{textDecoration:"none"}}>Try Demo ➝</a>
+            <a href={learnMore} style={{marginLeft:"50px",textDecoration:"none"}}>Learn More ➝</a>
+          </div>
+
+          <div className="mt-3">
+            <a href={googlePlay}>
+              <img src="/googlePlayBadge.svg" alt="Google Play Badge" />
+            </a>
+            <a href={appStore} style={{marginLeft:"20px"}}>
+              <img src="/appstoreBadge.svg" alt="App Store Badge" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LeftSection;
--- src/landing_page/products/ProductsPage.jsx
+++ src/landing_page/products/ProductsPage.jsx
@@ -6,14 +6,44 @@ import RightSection from "./RightSection";
 import Universe from "./Universe";
 
 function ProductPage() {
-    return ( 
-        <>
-            <Hero/>
-            <LeftSection/>
-            <RightSection/>
-            <Universe/>
-        </>
-     );
+  return (
+    <>
+      <Hero />
+      <LeftSection
+        imageURL="/kite.png"
+        productName="Kite"
+        productDescription="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection />
+
+      <LeftSection
+        imageURL="/coin.png"
+        productName="Coin"
+        productDescription="Buy direct mutual funds online, commission-free, delivered directly to your Demat account. Enjoy the investment experience on your Android and iOS devices."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+      <RightSection />
+
+      <LeftSection
+        imageURL="/varsity.png"
+        productName="Varsity mobile"
+        productDescription="An easy to grasp, collection of stock market lessons with in-depth coverage and illustrations. Content is broken down into bite-size cards to help you learn on the go."
+        tryDemo=""
+        learnMore=""
+        googlePlay=""
+        appStore=""
+      />
+
+      <Universe />
+    </>
+  );
 }
 
 export default ProductPage;
