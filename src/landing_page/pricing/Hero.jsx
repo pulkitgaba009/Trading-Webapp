41fe27a9bbe6a0299aa91cb75972a9b24edd0f5f
pulkitgaba009 <pulkitgaba009@gmail.com>
2025-07-06 15:23:38 +0530

    pricing component created

--- src/landing_page/pricing/Hero.jsx
+++ src/landing_page/pricing/Hero.jsx
@@ -1,7 +1,31 @@
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5 border-bottom text-center">
+        <h1 className="mt-5">Pricing</h1>
+        <h3 className="text-muted fs-4 mb-5">Free equity investments and flat ₹20 traay and F&O trades </h3>
+      </div>
+      <div className="row p-5 text-center">
+        <div className="col-4 p-4">
+          <img src="/pricingEquity.svg" alt="" />
+          <h1 className="fs-3">Free equity delivery</h1>
+          <p className="text-muted">All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
+        </div>
+
+        <div className="col-4 p-4">
+          <img src="/intradayTrades.svg" alt="" />
+          <h1 className="fs-3">Intraday and F&O trades</h1>
+          <p className="text-muted">Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p>
+        </div>
+
+        <div className="col-4 p-4">
+          <img src="/pricingEquity.svg" alt="" />
+          <h1 className="fs-3">Free direct MF</h1>
+          <p className="text-muted">All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- src/landing_page/pricing/PricingPage.jsx
+++ src/landing_page/pricing/PricingPage.jsx
@@ -1,12 +1,12 @@
-import Footer from "../Footer";
-import Navbar from "../Navbar";
 import Brokerage from "./Brokerage";
 import Hero from "./Hero";
+import OpenAccount from "../OpenAccount"
 
 function PricingPage() {
     return ( 
         <>
             <Hero/>
+            <OpenAccount/>
             <Brokerage/>
         </>
      );
